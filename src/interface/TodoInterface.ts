f65de58866877b1989327ed4f2ac7c043ddf9789
wlLima <lima14.wl@gmail.com>
2023-03-30 19:58:44 -0300

    feat: add styles and finished functions to todo list

---
+++ src/interface/TodoInterface.ts
@@ -0,0 +1,6 @@
+export interface ITodo{
+  id: string;
+  title: string;
+  description: string;
+  finished: boolean;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,48 @@ import styles from '@/styles/Home.module.css'
 import '@fontsource/roboto/400.css';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {Box, Paper, TextField, FormControl, Button, Checkbox, IconButton} from '@mui/material';
+import {TaskService} from '@/services/Tasks/TaskService';
+import { useEffect, useState } from 'react';
+import { ITodo } from '../interface/TodoInterface'
 
 
 export default function Home() {
+
+  const [todo, setTodo] = useState<ITodo[]>([])
+  const [title, setTitle] = useState('')
+
+  useEffect(()=>{
+    TaskService.list().then((response)=>{
+      setTodo(response.data)
+    })
+
+  },[todo])
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const data: Partial<ITodo> = {
+      title: title,
+      finished: false
+    }
+
+    const result = await TaskService.create(data).then((response)=>{
+      return response.data
+    })
+
+    setTodo([...todo, result])
+  }
+
+  const handleChangeCheck = async (event: React.ChangeEvent<HTMLInputElement>, data: ITodo) => {
+    data.finished = !data.finished
+    await TaskService.update(data)
+  };
+
+  const handleDelete = async (id: string) =>{
+    await TaskService.delete(id)
+  }
+
+
   return (
     <>
       <div className={styles.container}>
@@ -14,26 +53,43 @@ export default function Home() {
               <Box className={styles.boxContainer}>
                 <h1>To do list</h1>
                
+               <form onSubmit={(event)=> handleSubmit(event)} className={styles.formSubmit}>
+
                 <FormControl fullWidth sx={{ m: 1 }}>
-                  <TextField id="outlined-basic" label="Escreva uma tarefa" variant="outlined" />
+                  <TextField 
+                    id="outlined-basic" 
+                    label="Escreva uma tarefa" 
+                    variant="outlined" 
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setTitle(event.target.value)
+                      }
+                    } />
                 </FormControl>
                 
                 <FormControl fullWidth sx={{ m: 1 }}>
-                  <Button variant="contained">Adicionar</Button>
+                  <Button variant="contained" type='submit'>Adicionar</Button>
                 </FormControl>
-
-                <Box className={styles.boxTask}>
-                  <Checkbox />
-                  <span>Limpar a casa</span>
-                  <IconButton aria-label="delete" size="large">
-                    <DeleteIcon fontSize="inherit" />
-                  </IconButton>
-                </Box>
-               
-              </Box>
-
+               </form>
               
-            
+                {
+                  todo.length > 0 ? todo.map((todos: ITodo)=>{
+  
+                    return(
+                      <Box key={todos.id} className={styles.boxTask}>
+                        <Box>
+                          <Checkbox onChange={(event)=> handleChangeCheck(event, todos)}/>
+                          {todos.finished ? <span><s>{todos.title}</s></span> : <span>{todos.title}</span>}
+                        </Box>
+                        <IconButton aria-label="delete" size="large" onClick={()=> handleDelete(todos.id)}>
+                          <DeleteIcon fontSize="inherit"/>
+                        </IconButton>
+                      </Box>
+                    ) 
+                  }) : <h4>Nenhuma tarefa encontrada, adicione uma agora mesmo!</h4>
+                }
+               
+            </Box>
+
             </Paper>
          </Box>
       </div>
